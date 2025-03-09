c9bf27a77bded56943603325462e8269111162da
kydshub <kimsceysulit@gmail.com>
2025-03-09 16:21:42 +0800

    Bllank IonicAngular Project

---
+++ appdev/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.appdev',
+  appName: 'appdev',
+  webDir: 'www'
+};
+
+export default config;
