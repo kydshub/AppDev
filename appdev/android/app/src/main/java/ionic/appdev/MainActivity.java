61780f0c0422019b773c37bdafa9c84962e03716
kydshub <kimsceysulit@gmail.com>
2025-03-09 16:32:14 +0800

    Delete appdev directory

--- appdev/android/app/src/main/java/ionic/appdev/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package ionic.appdev;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- appdev/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ionic.appdev',
-  appName: 'appdev',
-  webDir: 'www'
-};
-
-export default config;
